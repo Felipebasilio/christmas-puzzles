8df84773c8e6150c7c6c0b508ee8df0dd2a4c03f
Felipe Basilio <fcb.97@hotmail.com>
2025-12-17 01:31:20 -0300

    awesome code again : )

--- 2/src/utils.js
+++ 2/src/utils.js
@@ -2,34 +2,74 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getIdsList = void 0;
 var read_file_1 = require("./read-file");
+var dividersMap = new Map([
+    [2, [2]],
+    [3, [3]],
+    [4, [2, 4]],
+    [5, [5]],
+    [6, [2, 3, 6]],
+    [7, [7]],
+    [8, [2, 4, 8]],
+    [9, [3, 9]],
+    [10, [2, 5, 10]],
+    [11, [11]],
+    [12, [2, 3, 4, 6, 12]],
+    [13, [13]],
+    [14, [2, 7, 14]],
+    [15, [3, 5, 15]],
+    [16, [2, 4, 8, 16]],
+    [17, [17]],
+    [18, [2, 3, 6, 9, 18]],
+    [19, [19]],
+    [20, [2, 4, 5, 10, 20]],
+    [21, [3, 7, 21]],
+    [22, [2, 11, 22]],
+    [23, [23]],
+    [24, [2, 3, 4, 6, 8, 12, 24]],
+    [25, [5, 25]],
+    [26, [2, 13, 26]],
+    [27, [3, 9, 27]],
+    [28, [2, 4, 7, 14, 28]],
+    [29, [29]],
+    [30, [2, 3, 5, 6, 10, 15, 30]],
+]);
 var getIdsList = function () {
+    var _a;
     var idsList = (0, read_file_1.readInputFile)();
     var duplicates = [];
     for (var _i = 0, idsList_1 = idsList; _i < idsList_1.length; _i++) {
         var idRange = idsList_1[_i];
-        var _a = idRange, start = _a.start, end = _a.end;
+        var _b = idRange, start = _b.start, end = _b.end;
         var difference = parseInt(end) - parseInt(start);
         var startIdNumber = parseInt(start);
         for (var i = 0; i <= difference; i++) {
-            var hasDuplicate = checkHasDuplicate(startIdNumber.toString());
-            if (hasDuplicate) {
-                duplicates.push(startIdNumber);
-                console.log("duplicates: ", duplicates);
+            var hasDuplicate = false;
+            for (var _c = 0, _d = (_a = dividersMap.get(startIdNumber.toString().length)) !== null && _a !== void 0 ? _a : []; _c < _d.length; _c++) {
+                var divider = _d[_c];
+                if (checkHasNthDuplicate(startIdNumber.toString(), divider)) {
+                    hasDuplicate = true;
+                    duplicates.push(startIdNumber);
+                    console.log("duplicates: ", duplicates);
+                    break;
+                }
             }
+            // if (hasDuplicate) {
+            //   duplicates.push(startIdNumber);
+            //   console.log("duplicates: ", duplicates);
+            // }
             startIdNumber++;
         }
     }
     return duplicates;
 };
 exports.getIdsList = getIdsList;
-var checkHasDuplicate = function (id) {
+var checkHasNthDuplicate = function (id, n) {
     var idLength = id.length;
-    if (idLength % 2 !== 0 || idLength === 1)
+    if (idLength === 1)
         return false;
-    var partOne = id.slice(0, idLength / 2);
-    var partTwo = id.slice(idLength / 2);
-    if (partOne === partTwo) {
-        return true;
-    }
-    return false;
+    var partsArray = Array.from({ length: n }, function (_, i) {
+        return id.slice(i * (idLength / n), (i + 1) * (idLength / n));
+    });
+    console.log("partsArray: ", partsArray);
+    return partsArray.every(function (part) { return part === partsArray[0] && part.length === partsArray[0].length; });
 };
--- 2/src/utils.ts
+++ 2/src/utils.ts
@@ -1,5 +1,37 @@
 import { IdRange, readInputFile } from "./read-file";
 
+const dividersMap = new Map<number, number[]>([
+  [2, [2]],
+  [3, [3]],
+  [4, [2, 4]],
+  [5, [5]],
+  [6, [2, 3, 6]],
+  [7, [7]],
+  [8, [2, 4, 8]],
+  [9, [3, 9]],
+  [10, [2, 5, 10]],
+  [11, [11]],
+  [12, [2, 3, 4, 6, 12]],
+  [13, [13]],
+  [14, [2, 7, 14]],
+  [15, [3, 5, 15]],
+  [16, [2, 4, 8, 16]],
+  [17, [17]],
+  [18, [2, 3, 6, 9, 18]],
+  [19, [19]],
+  [20, [2, 4, 5, 10, 20]],
+  [21, [3, 7, 21]],
+  [22, [2, 11, 22]],
+  [23, [23]],
+  [24, [2, 3, 4, 6, 8, 12, 24]],
+  [25, [5, 25]],
+  [26, [2, 13, 26]],
+  [27, [3, 9, 27]],
+  [28, [2, 4, 7, 14, 28]],
+  [29, [29]],
+  [30, [2, 3, 5, 6, 10, 15, 30]],
+]);
+
 export const getIdsList = (): number[] => {
   const idsList = readInputFile();
   let duplicates: number[] = [];
@@ -10,11 +42,18 @@ export const getIdsList = (): number[] => {
     let startIdNumber = parseInt(start);
 
     for (let i = 0; i <= difference; i++) {
-      let hasDuplicate = checkHasDuplicate(startIdNumber.toString());
-      if (hasDuplicate) {
-        duplicates.push(startIdNumber);
-        console.log("duplicates: ", duplicates);
+      let hasDuplicate = false;
+
+      for (const divider of dividersMap.get(startIdNumber.toString().length) ??
+        []) {
+        if (checkHasNthDuplicate(startIdNumber.toString(), divider)) {
+          hasDuplicate = true;
+          duplicates.push(startIdNumber);
+          console.log("duplicates: ", duplicates);
+          break;
+        }
       }
+          
       startIdNumber++;
     }
   }
@@ -22,17 +61,18 @@ export const getIdsList = (): number[] => {
   return duplicates;
 };
 
-const checkHasDuplicate = (id: string): boolean => {
+const checkHasNthDuplicate = (id: string, n: number): any => {
   const idLength = id.length;
 
-  if (idLength % 2 !== 0 || idLength === 1) return false;
+  if (idLength === 1) return false;
 
-  const partOne = id.slice(0, idLength / 2);
-  const partTwo = id.slice(idLength / 2);
+  const partsArray = Array.from({ length: n }, (_, i) =>
+    id.slice(i * (idLength / n), (i + 1) * (idLength / n))
+  );
 
-  if (partOne === partTwo) {
-    return true;
-  } 
-  
-  return false
+  console.log("partsArray: ", partsArray);
+
+  return partsArray.every(
+    (part) => part === partsArray[0] && part.length === partsArray[0].length
+  );
 };
